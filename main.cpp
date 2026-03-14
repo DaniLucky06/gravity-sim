64911e46b31760e9b91353f0c65cfaa164cfcae5
Daniel T14s <121113019+DaniLucky06@users.noreply.github.com>
2026-03-14 22:01:47 +0100

    implemented random restitution

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 
 const float g = 9.806;
 float timeScale = 1.e0f;
-float maxRest = 1.f;
+float minRest = .8f;
+float maxRest = .99f;
 
 const float windowScale = .5f;
 float FULLWIDTH = sf::VideoMode::getDesktopMode().width;
@@ -18,8 +19,8 @@ float pxToM = 1 / mToPx;
 
 // Number of balls
 int nBalls = 10;
-float minR = 10.f;
-float maxR = 200.f;
+float minR = 30.f;
+float maxR = 60.f;
 float maxV = 3.f;
 float density = 10.f;
 
@@ -32,7 +33,7 @@ struct Balls {
     std::vector<sf::Vector2f> velocities;
     std::vector<sf::CircleShape> circles;
 
-    void randomInit(sf::Window& window, int n, float minR, float maxR, float maxV, float density) {
+    void randomInit(sf::Window& window, int n, float minR, float maxR, float maxV, float density, float minRest, float maxRest) {
         radiuses.resize(n);
         masses.resize(n);
         colors.resize(n);
@@ -42,18 +43,18 @@ struct Balls {
         restitutions.resize(n);
         
         srand(time(0));
-        float iRAND_MAX = 1.f / RAND_MAX;
+        float fRAND_MAX = static_cast<float>(RAND_MAX);
         for (int i = 0; i < n; i++) {
-            float radius = (rand() * iRAND_MAX * (maxR - minR)) + minR;
+            float radius = (rand() / fRAND_MAX * (maxR - minR)) + minR;
             radiuses[i] = radius;
             masses[i] = radius * radius * M_PI * density;
             sf::Color color(rand()%256, rand()%256, rand()%256, rand()%256);
             colors[i] = color;
-            float X = rand() * iRAND_MAX;
-            float Y = rand() * iRAND_MAX;
+            float X = rand() / fRAND_MAX;
+            float Y = rand() / fRAND_MAX;
             positions[i] = (sf::Vector2f((X*(size.x - 2*radius) + radius), (Y*(size.y - 2*radius) + radius)) + static_cast<sf::Vector2f>(window.getPosition())) * pxToM;
-            velocities[i] = sf::Vector2f((rand()*iRAND_MAX*2.f - 1)*maxV, (rand()*iRAND_MAX*2.f - 1)*maxV);
-            restitutions[i] = 1.f;
+            velocities[i] = sf::Vector2f((rand()/fRAND_MAX*2.f - 1)*maxV, (rand()/fRAND_MAX*2.f - 1)*maxV);
+            restitutions[i] = (rand() / fRAND_MAX) * (maxRest - minRest) + minRest;
 
             // update circles
             circles[i].setRadius(radius);
@@ -69,13 +70,10 @@ int main(int, char**){
     sf::RenderWindow window(sf::VideoMode(size.x, size.y), "Bawls");
     sf::View fixedView(sf::FloatRect(0.f, 0.f, size.x, size.y));
     window.setView(fixedView);
-    sf::Vector2i windowPosPrev = window.getPosition();
-    sf::Vector2i windowPosNow = windowPosPrev;
-    sf::Vector2f windowVel(0.f, 0.f);
-    sf::Vector2f windowVelMet;
+    sf::Vector2i windowPos = window.getPosition();
 
     Balls balls{};
-    balls.randomInit(window, nBalls, minR, maxR, maxV, density);
+    balls.randomInit(window, nBalls, minR, maxR, maxV, density, minRest, maxRest);
 
     // time
     sf::Clock clock;
@@ -107,14 +105,7 @@ int main(int, char**){
         window.clear();
         
         // window dragging
-        windowPosNow = window.getPosition();
-
-        windowVel = sf::Vector2f(
-            (windowPosNow.x - windowPosPrev.x) / dt,
-            (windowPosNow.y - windowPosPrev.y) / dt
-        );
-        windowPosPrev = windowPosNow;
-        windowVelMet = windowVel * pxToM;
+        windowPos = window.getPosition();
         
         // physics
         for (int i = 0; i < nBalls; i++) {
@@ -131,26 +122,26 @@ int main(int, char**){
             // border collisions
             rest = balls.restitutions[i];
 
-            if (pxPos.x < radius + windowPosNow.x) {
+            if (pxPos.x < radius + windowPos.x) {
                 vel.x = -vel.x * rest;
-                pxPos.x = radius + windowPosNow.x;
-            } else if (pxPos.x > size.x + windowPosNow.x) {
+                pxPos.x = radius + windowPos.x;
+            } else if (pxPos.x > size.x + windowPos.x) {
                 vel.x = -vel.x * rest;
-                pxPos.x = size.x - radius + windowPosNow.x;
+                pxPos.x = size.x - radius + windowPos.x;
             }
-            if (pxPos.y < radius + windowPosNow.y)  {
+            if (pxPos.y < radius + windowPos.y)  {
                 vel.y = -vel.y * rest;
-                pxPos.y = radius + windowPosNow.y;
-            } else if (pxPos.y > size.y - radius + windowPosNow.y) {
+                pxPos.y = radius + windowPos.y;
+            } else if (pxPos.y > size.y - radius + windowPos.y) {
                 vel.y = -vel.y * rest;
-                pxPos.y = size.y - radius + windowPosNow.y;
+                pxPos.y = size.y - radius + windowPos.y;
             }
             pos = pxPos * pxToM;
             balls.positions[i] = pos;
             balls.velocities[i] = vel;
 
             // change render positions
-            balls.circles[i].setPosition(pxPos.x - windowPosNow.x, pxPos.y - windowPosNow.y);
+            balls.circles[i].setPosition(pxPos.x - windowPos.x, pxPos.y - windowPos.y);
             window.draw(balls.circles[i]);
         }
 
